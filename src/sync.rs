57fa7c0880cabebb5ca631ad1fd73d43ff213467
dvdsk <noreply@davidsk.dev>
2023-01-20 11:22:24 +0100

    moves route noeffect detect to block/unblock

--- src/sync.rs
+++ src/sync.rs
@@ -1,70 +1,12 @@
 use color_eyre::{
     eyre::{eyre, WrapErr},
-    Result, Section, SectionExt,
+    Result,
 };
 use itertools::{Either, Itertools};
-use std::{
-    collections::HashSet,
-    fs, io,
-    net::IpAddr,
-    process::{Command, Output},
-    str::FromStr,
-};
+use std::{collections::HashSet, fs, io, net::IpAddr, str::FromStr, thread, time::Duration};
 use trust_dns_resolver::error::{ResolveError, ResolveErrorKind};
 
-fn handle_any_error(output: Output, address: IpAddr, text: &'static str) -> Result<()> {
-    if !output.status.success() {
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        let stdout = String::from_utf8_lossy(&output.stdout);
-        Err(eyre!(text)
-            .with_section(move || stdout.trim().to_string().header("Stdout:"))
-            .with_section(move || stderr.trim().to_string().header("Stderr:"))
-            .with_section(move || format!("adress: {address}")))
-    } else {
-        Ok(())
-    }
-}
-
-fn block_route(address: IpAddr) -> Result<()> {
-    log::debug!("blocking: {address}");
-    let output = Command::new("route")
-        .arg("add")
-        .arg("-host")
-        .arg(address.to_string())
-        .arg("reject")
-        .output()
-        .wrap_err("Could not run route")?;
-    handle_any_error(output, address, "Command route add returned an error")
-}
-
-fn unblock_route(address: IpAddr) -> Result<()> {
-    log::debug!("unblocking: {address}");
-    let output = Command::new("route")
-        .arg("delete")
-        .arg("-host")
-        .arg(address.to_string())
-        .arg("reject")
-        .output()
-        .wrap_err("Could not run route")?;
-    handle_any_error(output, address, "Command route delete returned an error")
-}
-
-fn routing_table() -> Result<HashSet<IpAddr>> {
-    let output = Command::new("route")
-        .arg("-n")
-        .output()
-        .wrap_err("Could not run route")?;
-
-    let output = String::from_utf8_lossy(&output.stdout);
-    let routes: Result<HashSet<IpAddr>, _> = output
-        .lines()
-        .skip(2)
-        .map(|f| f.split_once(' ').unwrap().0)
-        .map(IpAddr::from_str)
-        .collect();
-    log::debug!("parsed routes: {routes:?}");
-    routes.wrap_err("Could not parse routing table entries")
-}
+mod route;
 
 const SYNC_BACKENDS: [&str; 9] = [
     "hwr-production-dot-remarkable-production.appspot.com",
@@ -125,52 +67,66 @@ fn sync_routes() -> Result<Vec<IpAddr>> {
     Ok(res.into_iter().collect())
 }
 
+/// directly after resuming from sleep the `route` tool does not seem to work
+/// therefore this retries `route` a few times
 pub fn block() -> Result<()> {
     log::info!("blocking sync");
-    let existing = routing_table().wrap_err("Error parsing routing table")?;
-    for addr in sync_routes()? {
-        if existing.contains(&addr) {
+    let to_block = sync_routes()?;
+
+    let mut attempt = 1;
+    let routes = route::table().wrap_err("Error parsing routing table")?;
+    for addr in &to_block {
+        if routes.contains(addr) {
             continue;
         }
 
-        // TODO enable when ip support for is_global lands
-        // if !addr.is_global() {
-        //     return Err(
-        //         eyre!("Tried to block local adress").with_note(|| format!("adress: {addr}"))
-        //     );
-        // }
-
-        block_route(addr)?;
+        loop {
+            match route::block(addr) {
+                Err(route::Error::NoEffect) if attempt > 4 => {
+                    return Err(eyre!("Timed out blocking"))
+                }
+                Err(route::Error::NoEffect) => {
+                    attempt += 1;
+                    thread::sleep(Duration::from_millis(200))
+                }
+                Err(other) => return Err(other).wrap_err("could not block route"),
+                Ok(()) => (),
+            }
+        }
     }
-    Ok(())
+
+    log::debug!("blocked successfull in {attempt} attemp(s)",);
+    return Ok(());
 }
 
 /// directly after resuming from sleep the `route` tool does not seem to work
 /// therefore this retries `route` a few times
 pub fn unblock() -> Result<()> {
     log::info!("unblocking sync");
-    let to_unblock: HashSet<_> = routes_from_file()
-        .wrap_err("Could not retrieve blocked routes from file")?
-        .into_iter()
-        .collect();
+    let to_unblock = routes_from_file().wrap_err("Could not retrieve blocked routes from file")?;
 
-    for attempt in 0..5 {
-        let routes = routing_table().wrap_err("Error parsing routing table")?;
-        for addr in to_unblock {
-            if routes.contains(&addr) {
-                unblock_route(addr)?;
-            } else {
-                debug!("confirmed addr: \"{addr}\" is unblocked");
-                to_unblock.remove(&addr);
+    let mut attempt = 1;
+    let routes = route::table().wrap_err("Error parsing routing table")?;
+    for addr in &to_unblock {
+        if !routes.contains(addr) {
+            continue;
+        }
+
+        loop {
+            match route::unblock(addr) {
+                Err(route::Error::NoEffect) if attempt > 4 => {
+                    return Err(eyre!("Timed out unblocking"))
+                }
+                Err(route::Error::NoEffect) => {
+                    attempt += 1;
+                    thread::sleep(Duration::from_millis(200))
+                }
+                Err(other) => return Err(other).wrap_err("could not unblock route"),
+                Ok(()) => (),
             }
         }
-
-        if to_unblock.is_empty() {
-            debug!("unblocked successfull in {attempt} attemps");
-            return Ok(());
-        }
-        thread::sleep(Duration::from_millis(200))
     }
 
-    Err(eyre!("Timed out unblocking"))
+    log::debug!("unblocked successfull in {attempt} attemp(s)",);
+    return Ok(());
 }
---
+++ src/sync/route.rs
@@ -0,0 +1,88 @@
+use color_eyre::{eyre, eyre::WrapErr, Help, Result, SectionExt};
+use std::{
+    collections::HashSet,
+    net::IpAddr,
+    process::{Command, Output},
+    str::FromStr,
+};
+
+fn handle_any_error(
+    output: Output,
+    address: &IpAddr,
+    text: &'static str,
+) -> std::result::Result<(), Error> {
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        Err(Error::Run(
+            eyre::eyre!(text)
+                .with_section(move || stdout.trim().to_string().header("Stdout:"))
+                .with_section(move || stderr.trim().to_string().header("Stderr:"))
+                .with_section(move || format!("adress: {address}")),
+        ))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn block(address: &IpAddr) -> std::result::Result<(), Error> {
+    log::debug!("blocking: {address}");
+    let output = Command::new("route")
+        .arg("add")
+        .arg("-host")
+        .arg(address.to_string())
+        .arg("reject")
+        .output()
+        .map_err(Error::Start)?;
+    handle_any_error(output, address, "Command route add returned an error")
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("could not run route program")]
+    Start(std::io::Error),
+    #[error("route run into an error while warning: {0}")]
+    Run(eyre::Report),
+    #[error("Could not verify change was applied: {0}")]
+    Verifying(eyre::Report),
+    #[error("Operation was not applied")]
+    NoEffect,
+}
+
+pub fn unblock(address: &IpAddr) -> std::result::Result<(), Error> {
+    log::debug!("unblocking: {address}");
+    let output = Command::new("route")
+        .arg("delete")
+        .arg("-host")
+        .arg(address.to_string())
+        .arg("reject")
+        .output()
+        .map_err(Error::Start)?;
+    handle_any_error(output, address, "Command route delete returned an error")?;
+
+    let routes = table()
+        .wrap_err("Error parsing routing table")
+        .map_err(Error::Verifying)?;
+    if routes.contains(address) {
+        Err(Error::NoEffect)
+    } else {
+        Ok(())
+    }
+}
+
+pub fn table() -> Result<HashSet<IpAddr>> {
+    let output = Command::new("route")
+        .arg("-n")
+        .output()
+        .wrap_err("Could not run route")?;
+
+    let output = String::from_utf8_lossy(&output.stdout);
+    let routes: Result<HashSet<IpAddr>, _> = output
+        .lines()
+        .skip(2)
+        .map(|f| f.split_once(' ').unwrap().0)
+        .map(IpAddr::from_str)
+        .collect();
+    log::debug!("parsed routes: {routes:?}");
+    routes.wrap_err("Could not parse routing table entries")
+}
